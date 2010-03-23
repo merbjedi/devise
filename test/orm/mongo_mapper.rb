dac1a655ac69885aa5507fd6b34c59eec360d06f
Jacques Crocker <railsjedi@gmail.com>
2010-03-23 09:13:02 -0700

    Protecting mongo_mapper from rails/test_help IntegrationTest patching nonsense

--- test/orm/mongo_mapper.rb
+++ test/orm/mongo_mapper.rb
@@ -1,8 +1,9 @@
+require File.expand_path('../../rails_app/config/environment', __FILE__)
+require 'action_dispatch/testing/integration'
+require 'rails/test_help'
+
 MongoMapper.database = "devise-test-suite"
 MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017)
-
-require File.expand_path('../../rails_app/config/environment', __FILE__)
-require 'rails/test_help'
 
 class ActiveSupport::TestCase
   setup do
